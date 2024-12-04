6334370ee78db03804fba5c47d359e3fc94e0a4b
triggerderler <respectrigger@gmail.com>
2024-12-04 20:49:47 +0300

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -4,11 +4,11 @@ Config.NPCs = {
     { 
         position = vector4(3802.91, 4464.94, 4.85, 119.38),
         job = "emlak1"
-    },
-    {
-        position = vector4(3796.17, 4462.4, 5.32, 117.55),
-        job = "emlak2"
     }
+--    {
+--        position = vector4(3796.17, 4462.4, 5.32, 117.55),
+--        job = "emlak2"
+--    }
 }
 
 Config.NPCModel = "s_m_y_dealer_01"

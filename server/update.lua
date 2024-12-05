861a3a6b84387a211339401c46315c98341a8681
triggerderler <respectrigger@gmail.com>
2024-12-05 13:58:51 +0300

    Update update.lua

--- server/update.lua
+++ server/update.lua
@@ -1,7 +1,8 @@
 local function checkVersion(err, githubContent, headers)
     local resourceName = GetCurrentResourceName()
     local installedVersion = GetResourceMetadata(resourceName, "version", 0)
-
+	local githubVersion = '0.0.0'
+	
     if err == 200 and githubContent then
         for line in githubContent:gmatch("[^\r\n]+") do
             if line:find("version") and not line:find("fx_version") then
@@ -14,6 +15,7 @@ local function checkVersion(err, githubContent, headers)
             print("^2[VERSION] ^7" .. resourceName .. " is up to date!")
         else
             print("^1[VERSION] ^7" .. resourceName .. " is outdated! Installed: " .. installedVersion .. ", GitHub: " .. githubVersion)
+			print("https://github.com/tr1ggerderler/td-moneywash")
         end
     else
         print("^3[VERSION] ^7Failed to fetch fxmanifest.lua from GitHub for resource: " .. resourceName)
